6b98ab3c1ada21f8fde1ef03e6fff4b3bd4d3016
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-12-01 19:05:36 +0600

    update code

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import './Brand.css';
 import google from '../../assets/google.png';
 import slack from '../../assets/slack.png';
 import atlassian from '../../assets/atlassian.png';
@@ -7,23 +5,23 @@ import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
 
 export default function Brand() {
-    return (
-        <div className="gtp3__brand section__padding">
-            <div>
-                <img src={google} alt="Google" />
-            </div>
-            <div>
-                <img src={slack} alt="slack" />
-            </div>
-            <div>
-                <img src={atlassian} alt="atlassian" />
-            </div>
-            <div>
-                <img src={dropbox} alt="dropbox" />
-            </div>
-            <div>
-                <img src={shopify} alt="shopify" />
-            </div>
-        </div>
-    );
+  return (
+    <div className='gtp3__brand section__padding'>
+      <div>
+        <img src={google} alt='Google' />
+      </div>
+      <div>
+        <img src={slack} alt='slack' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox' />
+      </div>
+      <div>
+        <img src={shopify} alt='shopify' />
+      </div>
+    </div>
+  );
 }
